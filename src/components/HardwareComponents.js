b1014af7fcf1c9d8dda99f7fbb8fc31eaa9c1d2a
ChristianFuchs <chris.fuchs88@gmail.com>
2022-09-27 18:13:32 +0200

    KBES22-5 clean up

--- src/components/HardwareComponents.js
+++ src/components/HardwareComponents.js
@@ -8,7 +8,7 @@ import Cookies from 'js-cookie';
 class HardwareComponents extends Component {
     constructor(props) {
         super(props);
-        this.state = {clients: [], message: null, keycloak: null, authenticated: false};
+        this.state = {clients: [], message: null, authenticated: false};
     }
 
     componentDidMount() {
@@ -25,43 +25,20 @@ class HardwareComponents extends Component {
             params: {
                 currencyParam: cookieCurrency
             }
-            //,
-            //responseType: 'blob'
           };
         
-        // axiosInstance.get('http://localhost:8081/hardwarecomponents', config, {params: {currencyParam: "EUR"}})
         axiosInstance.get('http://localhost:8081/hardwarecomponents', config)
         .then(response => this.setState({message: response.data}))
-        // .then(response => response.json)
-        // .then(data => this.setState({clients: data}));
-        console.log("this.state.message (componentDidMount): ",this.state.message)
     }
 
     render() {
+        if(keycloak.authenticated) {
+            return <div>check console</div>
+            // return <div>Message from Server = {this.state.message}</div>
         
-        const {clients, isLoading} = this.state;
-        console.log(this.state.message)
-    
-        if(this.state.keycloak) {
-
-            if(this.state.authenticated) {
-
-                if (isLoading) {
-                    return <p>Loading...</p>;
-                }
-                console.log("this.state.message (render): ",this.state.message)
-                let json = JSON.parse(this.state.message)
-                console.log("json: ",json)
-                
-                    return <div>Message from Server = {this.state.message}</div>
-
-            } else {
-                return <div>Unable to authenticate!</div>
-            }
+        } else {
+            return <div>Unable to authenticate!</div>
         }
-        return (
-            <div>check console</div>
-        );
     }
 }
 export default HardwareComponents;
--- src/components/Products.js
+++ src/components/Products.js
@@ -8,7 +8,7 @@ import Cookies from 'js-cookie';
 class Products extends Component {
     constructor(props) {
         super(props);
-        this.state = {clients: [], message: null, keycloak: null, authenticated: false};
+        this.state = {clients: [], message: null, authenticated: false};
     }
 
     componentDidMount() {
@@ -24,43 +24,21 @@ class Products extends Component {
             params: {
                 currencyParam: cookieCurrency
             }
-            //,
-            //responseType: 'blob'
           };
         
-        // axiosInstance.get('http://localhost:8081/hardwarecomponents', config, {params: {currencyParam: "EUR"}})
         axiosInstance.get('http://localhost:8081/products', config)
         .then(response => this.setState({message: response.data}))
-        // .then(response => response.json)
-        // .then(data => this.setState({clients: data}));
         console.log("this.state.message (componentDidMount): ",this.state.message)
     }
 
     render() {
-        
-        const {clients, isLoading} = this.state;
-        console.log(this.state.message)
-    
-        if(this.state.keycloak) {
+        console.log("this.state.message: ",this.state.message)
+        if(keycloak.authenticated) {
 
-            if(this.state.authenticated) {
-
-                if (isLoading) {
-                    return <p>Loading...</p>;
-                }
-                console.log("this.state.message (render): ",this.state.message)
-                let json = JSON.parse(this.state.message)
-                console.log("json: ",json)
-                
-                    return <div>Message from Server = {this.state.message}</div>
-
-            } else {
-                return <div>Unable to authenticate!</div>
-            }
+            return <div>check console</div>        
+        } else {
+            return <div>Unable to authenticate!</div>
         }
-        return (
-            <div>check console</div>
-        );
     }
 }
 export default Products;
